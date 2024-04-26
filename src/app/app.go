2e045a1bec51fe00fb987f7b09d69f5fa54e0ba7
lootensen <lootensen@proton.me>
2024-04-26 23:06:44 +0200

    chore: layout test

--- src/app/app.go
+++ src/app/app.go
@@ -42,7 +42,7 @@ func RunApp(jsInterface *component.JSInterface) {
 	}
 
 	closeBtn := &component.Button{
-		X:           300,
+		X:           250,
 		Y:           60,
 		Width:       60,
 		Height:      60,
